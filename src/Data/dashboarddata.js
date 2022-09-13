f8099f81cccec0cc4cc7719e42232ca5f7a80e34
Meenu <meenugigi2497@gmail.com>
2022-09-12 20:34:54 -0400

    save all files

---
+++ src/Data/dashboarddata.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        
+        img: "background.jpg",
+        title: "Meenu Gigi",
+        role: "Graduate Student",
+        github: "https://github.com/meenugigi",
+        linkedin: "https://www.linkedin.com/in/meenu-gigi-b18040129/",
+        download: "https://github.com/meenugigi/MyResume/raw/main/MeenuGigi_Resume.pdf",
+    },
+
+]
---
+++ src/Data/workexpdata.js
@@ -0,0 +1,21 @@
+export default [
+    
+    {   
+        gradertitle: "Computer Science Department Grader for CSCI 605 ",
+        graderdate: "(August 2022 - Present)",
+        graderrole: "Rochester Institute of Technology",
+        gradercourse: "Advanced Object Oriented Programming (Java)",
+        graderdesc: "Grading and evaluating students on the correctness and design for all codes submitted as a part of the course requirements. Concepts involved include Object oriented programming concepts, Inheritance, Abstract classes, threads, multi-threading, Java Generics, Stream API and Lambda Expressions",
+
+        worktitle: "Infinite Computing Systems Pvt Ltd  ",
+        workdate: "(July 2019 - July 2021)",
+        workrole: "Associate Software Engineer",
+        fulltimedescription: "Teamed with a cross-functional team in an Agile Development cycle to analyze requirements, develop, test product for a Label Design company. ",
+        data1: "Co-ordinated with team members to revamp features for a manufactoring and logistics application using Java, and executed test scripts in alignment with agile user stories and acceptance criteria.",
+        data2: "Created microservices, fixed and documented defects for a vessel tracking software.",
+        interntitle: "The D - Zone",
+        internrole: "Software Engineer Intern",
+        interndescription: "Analyzed client requirements and developed modules for a software using Java Spring framework in an Agile work enviroment."  
+    },
+    
+]
